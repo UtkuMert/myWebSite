a7a27f7020e19d8df57259539cd30b53daa99abb
UtkuMert <65029733+UtkuMert@users.noreply.github.com>
2021-05-22 21:58:32 +0300

    login page and validation control

---
+++ valid.js
@@ -0,0 +1,41 @@
+var kullaniciAdi = "utku@gmail.com";
+var password = "utku123"
+
+function Validation(){
+
+    var isimKontrol = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
+    var isim = document.forms["login-form"]["username"].value;
+    var sifre= document.forms["login-form"]["password"].value;
+
+    console.log(isim);
+    console.log(sifre);
+
+    if(isim == kullaniciAdi && sifre == password){
+        return true;
+    }
+
+    else if(isimKontrol.test(isim) == false) {
+         
+        alert('Lütfen Kullanıcı Adı Formatınızı Kontrol Edin.');
+        return false;
+        
+    }
+
+    else if(isim == null || isim == "")
+    {
+        alert("Lütfen isim kısmını boş bırakmayınız.");
+        return false;
+    }
+
+    else if(sifre == null || sifre == "")
+    {
+        alert("Lütfen şifre kısmını boş bırakmayınız.");
+        return false;
+    }
+    
+    else{
+        alert("Kullanıcı adı veya şifre hatalı");
+        return false;
+    }
+}
